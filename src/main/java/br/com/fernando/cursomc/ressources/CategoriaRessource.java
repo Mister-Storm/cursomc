d44d245cb48937505d9833c669736ec0da202077
Fernando Costa Leite <clfernando1979@hotmail.com>
2019-12-03 19:03:12 -0300

    Rest testado

---
+++ src/main/java/br/com/fernando/cursomc/ressources/CategoriaRessource.java
@@ -0,0 +1,15 @@
+package br.com.fernando.cursomc.ressources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaRessource {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String listar() {
+        return "REST está funcionando!!!";
+    }
+}
